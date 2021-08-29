8a56ce3814b2590e6f1a9757681543575fd461c4
Fadhil Radhian <fadhilradhian@gmail.com>
2021-08-29 13:46:01 +0700

    [ADD] mongodb connected

--- server.js
+++ server.js
@@ -1,19 +1,17 @@
 const express = require("express");
+const app = express();
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
+dotenv.config({ path: "config.env" });
 const morgan = require("morgan");
 const mysql = require("mysql");
 const path = require("path");
 const PORT = process.env.PORT || 1234;
 const route = require("./server/routes/router");
-
-dotenv.config({ path: "config.env" });
+const connectDB = require("./server/database/connection");
 
 //log requests
-const app = express();
 app.use(morgan("tiny"));
-
-//parse requests
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -23,6 +21,8 @@ app.use(route);
 app.listen(PORT, () => {
   console.log(`listening on ${PORT}`);
 });
+
+connectDB();
 
 // const con = mysql.createConnection({
 //    host: "localhost",
