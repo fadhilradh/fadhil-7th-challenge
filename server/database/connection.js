324e7cb3b1154671ab20a2741c778678d43b7cba
Fadhil Radhian <fadhilradhian@gmail.com>
2021-08-30 22:39:41 +0700

    handle env fallback

--- server/database/connection.js
+++ server/database/connection.js
@@ -2,12 +2,16 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const connection = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-      useCreateIndex: true,
-    });
+    const connection = await mongoose.connect(
+      process.env.MONGO_URI ||
+        "mongodb+srv://root:hamdalah@cluster0.g1t1n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true,
+      }
+    );
     console.log(`MongoDB Connected: ${connection.connection.host}`);
   } catch (err) {
     console.log(err);
--- server/services/__http.js
+++ server/services/__http.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 
 const http = axios.create({
-  baseURL: process.env.API_BASE_URL,
+  baseURL: process.env.API_BASE_URL || "http://localhost:5000/api/users",
 });
 
 exports.http = http;
